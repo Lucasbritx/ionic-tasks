8c4ec0d0586b680a7abd4a7e116f5bac57153a57
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-12 22:04:01 -0300

    feat: task list impl

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,60 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import { useState } from "react";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonList,
+  IonModal,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const addTask = () => {
+    if (newTask.trim()) {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+      setIsOpen(false);
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>My Tasks</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+      <IonContent className="ion-padding">
+        <IonButton onClick={() => setIsOpen(true)}>Add Task</IonButton>
+
+        <IonList>
+          {tasks.map((task: any, i: number) => (
+            <IonItem key={i}>{task}</IonItem>
+          ))}
+        </IonList>
+
+        <IonModal isOpen={isOpen} onDidDismiss={() => setIsOpen(false)}>
+          <IonContent className="ion-padding">
+            <IonInput
+              value={newTask}
+              placeholder="Enter task"
+              onIonChange={(e) => setNewTask(e.detail.value!)}
+            />
+            <IonButton expand="full" onClick={addTask}>
+              Save
+            </IonButton>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
